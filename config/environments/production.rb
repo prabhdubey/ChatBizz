12fa489af15357020e53e944ac0bce9e49daf716
prabhdubey <prabh131339@gmail.com>
2016-08-24 08:53:14 +0530

    Updated Development.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.web_socket_server_url = "wss://ocp.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://ocp.herokuapp.com', 'http://ocp.herokuapp.com']
-
+ 
 end
